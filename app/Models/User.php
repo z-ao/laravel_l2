c21bde79dcde86a053a5858aac96fd81a5b20965
Lea <452224689@qq.com>
2018-10-05 20:08:24 +0800

    add#permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 use Auth;
 
 class User extends Authenticatable
 {
+    use HasRoles;
+
     use Notifiable {
         notify as protected laravelNotify;
     }
